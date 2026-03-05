f0aa0f7bcfcd01c41f7a3ceee3f774bab44f4621
Samir Karimov <pragmatecheduaz@gmail.com>
2026-03-05 11:14:03 +0400

    Email: switch from Zoho SMTP to Resend API (anymail)

--- vaam_project/settings.py
+++ vaam_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+    'anymail',
     'core',
     'dashboard',
 ]
@@ -159,20 +160,20 @@ else:
     }
 
 # ============ EMAIL ============
-EMAIL_BACKEND = os.environ.get(
-    'EMAIL_BACKEND',
-    'django.core.mail.backends.console.EmailBackend' if DEBUG else 'django.core.mail.backends.smtp.EmailBackend'
-)
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ('true', '1', 'yes')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@vaamtrading.com')
-CONTACT_NOTIFICATION_EMAIL = os.environ.get('CONTACT_NOTIFICATION_EMAIL', 'info@vaamtrading.com')
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'anymail.backends.resend.EmailBackend'
+
+ANYMAIL = {
+    'RESEND_API_KEY': os.environ.get('RESEND_API_KEY', ''),
+}
+
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'VAAM Global <info@vaamglobal.com>')
+CONTACT_NOTIFICATION_EMAIL = os.environ.get('CONTACT_NOTIFICATION_EMAIL', 'info@vaamglobal.com')
 
 # MANAGERS receives mail_managers() notifications (contact form, inquiries)
-_notify_email = os.environ.get('CONTACT_NOTIFICATION_EMAIL', 'info@vaamtrading.com')
+_notify_email = os.environ.get('CONTACT_NOTIFICATION_EMAIL', 'info@vaamglobal.com')
 MANAGERS = [('VAAM', _notify_email)]
 ADMINS = MANAGERS
 
